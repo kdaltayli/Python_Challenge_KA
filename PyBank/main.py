d1186197facc2b4469a08d7cb9f4c0ead57b25c8
Kursad Altayli <kdaltayli@gmail.com>
2020-06-25 23:18:49 -0400

    Completed but needs to be developed in style

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,69 @@
-main_script
+# main_script
+import os
+import csv
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+csv_path=os.path.join('Resources','budget_data.csv')
+#prev_revenue=row[1]
+
+revenue_change_list=[]
+total_amount=0.0
+length_of_month=[]
+profit=[]
+total_revenue=0.0
+months=[]
+
+with open(csv_path) as csvfile:
+        csv_reader=csv.reader(csvfile, delimiter=',')
+       
+        csv_header=next(csv_reader)
+        first_row = next(csv_reader)
+        length_of_month.append(first_row[0])
+
+        total_revenue=float(first_row[1])
+
+        prev_revenue = int(first_row[1])
+        for row in csv_reader:
+
+            months.append(row[0])
+            length_of_month.append(row[0])
+
+            profit.append(row[1])
+            
+            total_revenue=int(total_revenue)+int(row[1])
+           
+            revenue_change=int(row[1])-prev_revenue
+
+            prev_revenue=int(row[1])
+            
+            # revenue_change_list=revenue_change_list + [revenue_change]
+            revenue_change_list.append(revenue_change)
+        
+        Greatest_increase=max(revenue_change_list)
+        Greatest_decrease=min(revenue_change_list)
+            
+        max_index = revenue_change_list.index(Greatest_increase)
+        greatest_increase_date=months[max_index]
+
+        min_index=revenue_change_list.index(Greatest_decrease)
+        greatest_decrease_date=months[min_index]
+        
+         
+revenue_average=round(sum(revenue_change_list) / len(revenue_change_list),2)
+print("FINANCIAL ANALYSIS")
+
+print("------------------------")
+
+print(f"Total Months is  {len(length_of_month)} \n"
+f"The Total Revenue is :  {total_revenue} \n"
+f"Revenue Average is {revenue_average}\n"
+f"Greatest Increase in Profits {greatest_increase_date} {Greatest_increase}\n"
+f"Greatest Decrease in Profits {greatest_decrease_date}  {Greatest_decrease}")
+
+
+
+
+
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,58 @@
-main_script
+# main_script for PyPoll
+
+import os
+import csv
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+csv_path=os.path.join('Resources','election_data.csv')
+
+def percentage(part,whole):
+    return round(100 * float(part)/float(whole),2)
+
+total_vote=[]
+candidates=["Khan","Li","Correy","O'Tooley"]
+candidates_total=[]
+total_Khan=0.0
+total_Li=0.0
+total_Correy=0.0
+total_OTooley=0.0
+with open(csv_path) as csvfile:
+
+    csv_reader=csv.reader(csvfile,delimiter=',')
+    csv_header=next(csvfile)
+
+    for row in csv_reader:
+        total_vote.append(row[1])
+
+        if row[2]==candidates[0]:
+            total_Khan=total_Khan+1
+            
+        elif row[2]==candidates[1]:
+            total_Li=total_Li+1
+            
+        elif row[2]==candidates[2]:
+            total_Correy=total_Correy+1
+            
+        elif row[2]==candidates[3]:
+            total_OTooley=total_OTooley+1
+            
+candidates_total.append(total_Khan)
+candidates_total.append(total_Li)
+candidates_total.append(total_Correy)
+candidates_total.append(total_OTooley)
+
+# greatest_vote=max(total_Khan,total_Li,total_OTooley,total_Correy)
+greatest_vol=max(candidates_total)
+max_index=candidates_total.index(greatest_vol)
+winner=candidates[max_index]
+
+print("ELECTION RESULTS")
+print("------------------")
+print(f"The total Votes is   {len(total_vote)}")
+print("------------------")
+print(f"Total Khan % is {percentage(total_Khan,len(total_vote))} {total_Khan}\n"
+    f"Total Li is {percentage(total_Li,len(total_vote))}  {total_Li}\n"
+    f"Total Correy is {percentage(total_Correy,len(total_vote))} {total_Correy}\n"
+    f"Total OTooley is {percentage(total_OTooley,len(total_vote))} {total_OTooley}")
+print("------------------")
+print("The Winner is .....     " + winner)
