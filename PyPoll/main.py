d3602c3c75792af9f2b1b0179184e8049739a629
Kursad Altayli <kdaltayli@gmail.com>
2020-06-27 20:40:43 -0400

    updated one

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,7 +13,9 @@ total_vote=0.0
 
 # created candidate dictionary
 candidates_dict={}
-
+candidate_list=[]
+percentage_list=[]
+candidate_vote=[]
 with open(csv_path) as csvfile:
 
     csv_reader=csv.reader(csvfile,delimiter=',')
@@ -35,14 +37,19 @@ print(f"ELECTION RESULTS\n"
     f"-----------------------------")
 
 
-
+# candidates_dict.keys[candidates_dict.values.index("max_value")]
 for candidate in candidates_dict.keys():
 
     percentage_candidate=percentage(candidates_dict[candidate],total_vote)
-
+    candidate_list.append(candidate)
+    percentage_list.append(percentage_candidate)
+    candidate_vote.append(candidates_dict[candidate])
     print(f"{candidate} {percentage_candidate}% ({candidates_dict[candidate]})")
 
+result_file=zip(candidate_list,percentage_list,candidate_vote)
+
 max_value=max(candidates_dict.values())
+
 max_keys=[k for k, candidate in candidates_dict.items() if candidate==max_value]
 
 print(f"-----------------------------\n"
@@ -56,5 +63,11 @@ with open(output_path,'w') as text:
     f"Total Votes: {total_vote}\n"
     f"-----------------------------")
     text.write("--------------------\n")
-    text.write(f" {candidates_dict} \n"
-            f"The Winner is {max_keys} ({max_value}) ")
+    for row in zip(result_file):
+        
+        a,b,c=row[0]
+        print(a)
+        final_result=(f" {a}{b}% ({c}) \n ")
+        text.write(f" {final_result}")
+    text.write("--------------------\n")
+    text.write(f"The Winner is {max_keys} ({max_value}) ")
